541ce252ed49289c2d6bb2292a18602bd4530ff6
lewismck <lewisamckeown@gmail.com>
2015-12-11 17:25:59 +0000

    Increased modifiers added fadeIn for each new jd
    Increased the modifiers using an AGILE SCRUM workflow. Added fadeIn for
    each new jd to make it look a bit fancier

--- jobs.js
+++ jobs.js
@@ -6,9 +6,11 @@ function generateJobDesc(){
                   "Our creative and UX teams are eager to work alongside", "A global provider of financial news, data and decision assistance tools are seeking",
                   "We will offer fantastic career progression, along with set training for new hires to develop into", "The company prides itself on not being your stereotypical company, instead we're made of",
                   "Good knowledge and understanding of common design patterns and OO principles, basically be",
-                  "Would you like the opportunity to work on complex, globally used enterprise grade Cloud Software platform? Then apply now we're looking for"];
-  var languages = [" Ruby"," Javascript", " CSS", " Python", " mySQL", " Java", " C++", " Perl", " erlang", " Prolog", " C", " Haskell", " HTML5", " PHP", " VB", " GO"];
-  var modifiers = [" ninjas", " wizards", " gurus", " space children", " unicorns", " masters", " dominatrixes", " fetishists", " over enthusiasts", " dragon tamers", " super Saiyan level 3's"];
+                  "Would you like the opportunity to work on complex, globally used enterprise grade Cloud Software platform? Then apply now we're looking for",
+                  "Our team of growth hacking digital marketing gods must be joined on mount SEOlympus by",
+                  "Good team working skills are a must. This is a highly collaborative, idea sharing & Agile environment welcoming new"];
+  var languages = [" Ruby"," Javascript", " CSS", " Python", " mySQL", " Java", " C++", " Perl", " erlang", " Prolog", " C", " Haskell", " HTML5", " PHP", " VB", " GO", " Angular", " React", " SCALA"];
+  var modifiers = [" ninjas", " wizards", " gurus", " space children", " unicorns", " masters", " dominatrixes", " fetishists", " over enthusiasts", " dragon tamers", " super Saiyan level 3's", " Star Lords", " Nazgul"];
   var obligatory = " Great organisation and problem solving skills are also key.";
 //generate the random elements
   var lang = languages[Math.floor(Math.random() * languages.length)];
@@ -19,5 +21,6 @@ function generateJobDesc(){
 //  var snip2 = snippet[Math.floor(Math.random() * snippet.length)];
 
 //spit out a great HR appealing jd
-  $("#jd").html(snip + "<strong>" + lang + mod + "</strong>.<br><small>" + obligatory +"</small>");
+//hide, then fadein because that's the only way to get fadeIn to work on already visible elements apparently
+  $("#jd").hide().html(snip + "<strong>" + lang + mod + "</strong>.<br><small>" + obligatory +"</small>").fadeIn(200);
 }
