c6516299c27a3577ea25bca6def1bc5d84fafd63
lewismck <lewisamckeown@gmail.com>
2015-12-12 12:54:45 +0000

    More variety in generated descriptions
    Added more variety in the generated descriptions by making closing
    snippets. Also added a random integer function for helping choose which
    type of snippet to use … uhh… rapid iteration

--- jobs.js
+++ jobs.js
@@ -1,7 +1,7 @@
 //Pick randomly from a list of langauges and modifiers and return a pair
 function generateJobDesc(){
 //Array lists of lanaguages, jd snippets and modifier words (plus obligatory final sentence)
-  var snippet = ["We are looking for Fresh Graduates who are", "Join our team of ambitious and upbeat", "The next members of our team will ideally be", "Our robust, agile, enterprise web solutions are built inhouse by",
+  var startSnippet = ["We are looking for Fresh Graduates who are", "Join our team of ambitious and upbeat", "The next members of our team will ideally be", "Our robust, agile, enterprise web solutions are built inhouse by",
                   "To help develop and build dynamic web based software solutions we require some ", "This exciting, innovative company is looking for people willing to develop themselves into",
                   "Our creative and UX teams are eager to work alongside", "A global provider of financial news, data and decision assistance tools are seeking",
                   "We will offer fantastic career progression, along with set training for new hires to develop into", "The company prides itself on not being your stereotypical company, instead we're made of",
@@ -9,18 +9,40 @@ function generateJobDesc(){
                   "Would you like the opportunity to work on complex, globally used enterprise grade Cloud Software platform? Then apply now we're looking for",
                   "Our team of growth hacking digital marketing gods must be joined on mount SEOlympus by",
                   "Good team working skills are a must. This is a highly collaborative, idea sharing & Agile environment welcoming new"];
+  var closeSnippet = [" needed for a global brand, who are market leaders in their field",
+                      " needed to join our teams working on cutting-edge Service Orientated Architecture based solutions for the cloud community",
+                      " willing to embrace the challenge of working in an agile development team",
+                      " Needed to devise possible solutions to predicted problems, whilst also evaluating other options",
+                      " Required with a good understanding of n-tier development approaches",
+                      " wanted. Preferrably with a degree in Software Engineering or Computer Science",
+                      " are desired with an awareness of Service Orientated Architectures"];
   var languages = [" Ruby"," Javascript", " CSS", " Python", " mySQL", " Java", " C++", " Perl", " erlang", " Prolog", " C", " Haskell", " HTML5", " PHP", " VB", " GO", " Angular", " React", " SCALA"];
   var modifiers = [" ninjas", " wizards", " gurus", " space children", " unicorns", " masters", " dominatrixes", " fetishists", " over enthusiasts", " dragon tamers", " super Saiyan level 3's", " Star Lords", " Nazgul"];
   var obligatory = " Great organisation and problem solving skills are also key.";
+
 //generate the random elements
   var lang = languages[Math.floor(Math.random() * languages.length)];
   var mod = modifiers[Math.floor(Math.random() * modifiers.length)];
-  var snip = snippet[Math.floor(Math.random() * snippet.length)];
-//  var lang2 = languages[Math.floor(Math.random() * languages.length)];
-//  var mod2 = modifiers[Math.floor(Math.random() * modifiers.length)];
-//  var snip2 = snippet[Math.floor(Math.random() * snippet.length)];
+  var snip = startSnippet[Math.floor(Math.random() * startSnippet.length)];
+  var closeSnip = closeSnippet[Math.floor(Math.random() * closeSnippet.length)];
+ // generate a random number to decide how to put the elements together
+  var decider = getRandomInt(1, 100);
 
 //spit out a great HR appealing jd
 //hide, then fadein because that's the only way to get fadeIn to work on already visible elements apparently
-  $("#jd").hide().html(snip + "<strong>" + lang + mod + "</strong>.<br><small>" + obligatory +"</small>").fadeIn(200);
+if( decider % 2 == 0){
+  $("#jd").hide().html(snip + "<strong>" + lang + mod + "</strong>.<br><small>" + obligatory + "</small>").fadeIn(200);
+}
+else {
+  $("#jd").hide().html("<strong>" + lang + mod + "</strong>" + closeSnip + ".<br><small>" + obligatory + "</small>").fadeIn(200);
+}
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ * From Mozilla developer library
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
